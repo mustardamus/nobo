841b2ee6d715c6267cd982b14374c170554faeea
Sebastian Senf <contact@mustardamus.com>
2010-07-18 00:10:44 +0200

    stepped up the initial testing

--- test/nobo.js
+++ test/nobo.js
@@ -5,18 +5,20 @@ var config  = require('../config'),
     sys     = require('sys');
 
 
-assert.response = function(url, statusCode, contentType) {
+assert.response = function(url, statusCode, contentType, inBody) {
   request({ uri: 'http://localhost:'+config.port+url }, function(error, response, body) {
     if(error) throw new Error(error);
     
     assert.equal(response.statusCode, statusCode);
     assert.equal(response.headers['content-type'], contentType);
-    sys.puts('OK: '+url+' '+statusCode+' '+contentType);
+    assert.notEqual(body.indexOf(inBody), -1);
+    sys.puts('OK: '+url+' '+statusCode+' '+contentType+' "'+inBody+'" found in body');
   });
 }
 
-assert.response('/', 200, 'text/html');
-assert.response('/index.html', 200, 'text/html');
-assert.response('/css/reset.css', 200, 'text/css');
-assert.response('/css/app.less', 200, 'text/css');
-assert.response('/404', 404, 'text');
+assert.response('/', 200, 'text/html', 'something great');
+assert.response('/index.html', 200, 'text/html', 'something great');
+assert.response('/css/reset.css', 200, 'text/css', 'html,body');
+assert.response('/css/app.less', 200, 'text/css', 'background: #eeeeee;');
+assert.response('/js/app.js', 200, 'text/javascript', 'ready');
+assert.response('/notexistent', 404, 'text', '404');
