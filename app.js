c2c4e6865d7624ff1c43da1fd6a296af0ef164c5
Sebastian Senf <contact@mustardamus.com>
2010-07-18 03:58:24 +0200

    the road to .post

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ nobo.get('/hello', function(request, response) {
   response.sendHtml('World!');
 });
 
+nobo.get('/say', function(request, response) {
+  response.sendJson('{"you wanted me to say":'+request.params.word+'}');
+});
+
 nobo.fire();
